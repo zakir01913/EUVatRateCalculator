b78caedb03394b918651dfd7274a019bc166c482
Zakir Hossain <zakir01913@gmail.com>
2018-10-06 22:42:38 +0600

    Delete Unused file

--- app/src/main/java/com/zakir/euvatcalculation/domain/executor/PostExecutionThread.java
+++
@@ -1,7 +0,0 @@
-package com.zakir.euvatcalculation.domain.executor;
-
-import io.reactivex.Scheduler;
-
-public interface PostExecutionThread {
-    Scheduler getScheduler();
-}
--- app/src/main/java/com/zakir/euvatcalculation/domain/executor/ThreadExecutor.java
+++
@@ -1,6 +0,0 @@
-package com.zakir.euvatcalculation.domain.executor;
-
-import java.util.concurrent.Executor;
-
-public interface ThreadExecutor extends Executor {
-}
