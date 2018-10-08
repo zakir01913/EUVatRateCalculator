0300d00fd44e6d5518ce41fcb0faf74a1512f86c
Zakir Hossain <zakir01913@gmail.com>
2018-10-08 13:50:48 +0600

    Added EmptyEUVatRateListException and  NetworkConnectionException exception classes.

---
+++ app/src/main/java/com/zakir/euvatcalculation/data/exception/EmptyEUVatRateListException.java
@@ -0,0 +1,10 @@
+package com.zakir.euvatcalculation.data.exception;
+
+public class EmptyEUVatRateListException extends Exception  {
+    public EmptyEUVatRateListException() {
+    }
+
+    public EmptyEUVatRateListException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ app/src/main/java/com/zakir/euvatcalculation/data/exception/NetworkConnectionException.java
@@ -0,0 +1,12 @@
+package com.zakir.euvatcalculation.data.exception;
+
+public class NetworkConnectionException extends Exception {
+
+  public NetworkConnectionException() {
+    super();
+  }
+
+  public NetworkConnectionException(final Throwable cause) {
+    super(cause);
+  }
+}
