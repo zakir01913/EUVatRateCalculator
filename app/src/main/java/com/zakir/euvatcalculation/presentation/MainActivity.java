74bc49cca06d637facfe3078802f63373e3515c0
Zakir Hossain <zakir01913@gmail.com>
2018-10-08 19:56:02 +0600

    Implemented UI functionality to send country, rate and amount changes to presenter.

--- app/src/main/java/com/zakir/euvatcalculation/presentation/MainActivity.java
+++ app/src/main/java/com/zakir/euvatcalculation/presentation/MainActivity.java
@@ -2,13 +2,23 @@ package com.zakir.euvatcalculation.presentation;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.zakir.euvatcalculation.R;
 import com.zakir.euvatcalculation.di.component.DaggerApplicationComponent;
 import com.zakir.euvatcalculation.domain.model.VatTypeRate;
+import com.zakir.euvatcalculation.utils.EditTextCurrencyInputFilter;
 
 import java.util.List;
 
@@ -24,6 +34,12 @@ public class MainActivity extends AppCompatActivity implements VatCalculatorCont
 
     @BindView(R.id.country_sp)
     Spinner countrySpinner;
+    @BindView(R.id.rate_rg)
+    RadioGroup rateRadioGroup;
+    @BindView(R.id.total_amount_tv)
+    TextView totalAmountTextView;
+    @BindView(R.id.currency_et)
+    TextInputEditText textInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +50,36 @@ public class MainActivity extends AppCompatActivity implements VatCalculatorCont
         DaggerApplicationComponent.builder().build().inject(this);
         presenter.setView(this);
         presenter.loadVatData();
+
+        countrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                presenter.onCountryChange(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        rateRadioGroup.setOnCheckedChangeListener((radioGroup, i) -> presenter.onRateTypeChange(i));
+        textInputEditText.setFilters(new InputFilter[]{ new EditTextCurrencyInputFilter()});
+        textInputEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                presenter.onAmountChange(editable.toString());
+            }
+        });
     }
 
     @Override
@@ -55,12 +101,22 @@ public class MainActivity extends AppCompatActivity implements VatCalculatorCont
 
     @Override
     public void updateRateSelection(List<VatTypeRate> vatTypeRates) {
-
+        rateRadioGroup.removeAllViews();
+        for (int i = 0; i < vatTypeRates.size(); i++) {
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setLayoutParams(new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT));
+            radioButton.setId(i);
+            radioButton.setText(vatTypeRates.get(i).getName());
+            if (i == 0) {
+                radioButton.setChecked(true);
+            }
+            rateRadioGroup.addView(radioButton);
+        }
     }
 
     @Override
-    public void updateTotalAmount(float totalAmount) {
-
+    public void updateTotalAmount(String totalAmount) {
+        totalAmountTextView.setText(totalAmount);
     }
 
     @Override
